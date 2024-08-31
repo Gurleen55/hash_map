e8903a4026a4276f4d97e5a4704c12508da242b6
Gurleen <gurleensinghdalli@gmail.com>
2024-08-31 01:01:03 -0300

    finished Hashtable implementation
    added has?, remove, length, clear, keys, values and entries methods

--- main.rb
+++ main.rb
@@ -51,17 +51,41 @@ class LinkedList
       current_node = current_node.next_node
     end
   end
+
+  def remove(key)
+    current_node = first_node
+
+    while current_node
+      if current_node.key == key
+        if current_node == first_node
+          @first_node = current_node.next_node
+          @first_node.previous_node = nil if @first_node
+          @last_node = nil unless @first_node # Update @last_node if list is now empty
+        elsif current_node == last_node
+          @last_node = current_node.previous_node
+          @last_node.next_node = nil
+        else
+          current_node.previous_node.next_node = current_node.next_node
+          current_node.next_node.previous_node = current_node.previous_node
+        end
+        break # Assuming we want to remove only the first occurrence
+      end
+
+      current_node = current_node.next_node
+    end
+  end
 end
 
 # class that will contain code for hash table
 class HashMap
-  attr_accessor :keys
+  attr_accessor :keys, :values
 
   def initialize
     @capacity = 16
     @bucket = Array.new(@capacity) { LinkedList.new }
     @load_factor = 0.75
     @keys = []
+    @values = []
   end
 
   def hash(key)
@@ -85,6 +109,7 @@ class HashMap
       @bucket[bucket_number].append(key, value)
       @keys << key
     end
+    @values << value
   end
 
   def grow_buckets_if_needed
@@ -119,8 +144,54 @@ class HashMap
     node = @bucket[bucket_number].find_node(key)
     node ? node.value : nil
   end
+
+  def has?(key)
+    bucket_number = hash(key) % @capacity
+    node = @bucket[bucket_number].find_node(key)
+    !!node
+  end
+
+  def remove(key)
+    return nil unless @keys.include?(key)
+
+    bucket_number = hash(key) % @capacity
+    value = @bucket[bucket_number].find_node(key).value
+    @values.delete(value)
+    @bucket[bucket_number].remove(key)
+    @keys.delete(key)
+  end
+
+  def length
+    @keys.size
+  end
+
+  def clear
+    @capacity = 16
+    @bucket = Array.new(@capacity) { LinkedList.new }
+    @load_factor = 0.75
+    @keys = []
+  end
+
+  def entries
+    entries = []
+    @keys.each_with_index { |key, index| entries << [key, @values[index]] }
+    entries
+  end
 end
 
-hash = HashMap.new
-hash.set("Gurleen", "singh")
-p hash.get("Gurleen")
+test = HashMap.new
+test.set("apple", "red")
+test.set("banana", "yellow")
+test.set("carrot", "orange")
+test.set("dog", "brown")
+test.set("elephant", "gray")
+test.set("frog", "green")
+test.set("grape", "purple")
+test.set("hat", "black")
+test.set("ice cream", "white")
+test.set("jacket", "blue")
+test.set("kite", "pink")
+test.set("lion", "golden")
+test.set("moon", "silver")
+
+p test.entries
